5e1844b7fa4f20e185cf1bfdda74bc41230baf55
cade-kreikemeier <cade@DESKTOP-70LMLPQ.localdomain>
2021-10-12 17:31:25 -0600

    Moved jobs back into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,33 @@ import { useState } from 'react';
 import { supabase } from './client';
 import Board from './Board/Board';
 
+
 interface Customer {
   id: number,
   created_at: string,
   name: string
 };
 
+interface Jobs {
+  id: number,
+  created_at: string,
+  customer_id: number,
+  customer_po: string,
+  part_number: string,
+  part_name: string,
+  part_rev: string,
+  drawing_number: string,
+  drawing_rev: string,
+  quantity: number,
+  material_id: number,
+  current_process: number,
+  processes: number[],
+  due_date: string
+};
+
 function App() {
   const [customers, setCustomers] = useState<Customer[] | null>([]);
-
-  useEffect(() => {
-    fetchCustomers();
-  }, [])
+  const [jobs, setJobs] = useState<Jobs[] | null>([]);
 
   async function fetchCustomers() {
     const { data } = await supabase
@@ -24,6 +39,22 @@ function App() {
     setCustomers(data);
     console.log('data: ', data);
   };
+
+  async function fetchJobs() {
+    const { data } = await supabase
+      .from<Jobs>('job_numbers')
+      .select('*');
+    setJobs(data);
+    console.log('jobs: ', data);
+  };
+
+  useEffect(() => {
+    fetchCustomers();
+  }, [])
+
+  useEffect(() => {
+    fetchJobs();
+  }, [])
 
   return (
     <div className="App">
--- src/Board/Board.tsx
+++ src/Board/Board.tsx
@@ -3,41 +3,11 @@ import './Board.css';
 import { supabase } from "../client";
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
-interface Jobs {
-  id: number,
-  created_at: string,
-  customer_id: number,
-  customer_po: string,
-  part_number: string,
-  part_name: string,
-  part_rev: string,
-  drawing_number: string,
-  drawing_rev: string,
-  quantity: number,
-  material_id: number,
-  current_process: number,
-  processes: number[],
-  due_date: string
-};
 
 function Board() {
-  const [jobs, setJobs] = useState<Jobs[] | null>([]);
-
-  async function fetchJobs() {
-    const { data } = await supabase
-      .from<Jobs>('job_numbers')
-      .select('*');
-    setJobs(data);
-    console.log('jobs: ', data);
-  };
-
-  useEffect(() => {
-    fetchJobs();
-  }, [])
-
   return (
     <div className="board"></div>
   )
-}
+};
 
-export default Board
+export default Board;
