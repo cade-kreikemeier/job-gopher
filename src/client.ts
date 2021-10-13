a5c0bb40fa9bd00881175e638e9e8b1f0c509017
cade-kreikemeier <cade@DESKTOP-70LMLPQ.localdomain>
2021-10-13 15:29:42 -0600

    small changes

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-
 export const supabase = createClient(
   'https://ymoesikqdsfnkzmimecb.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNDA2NDgxMiwiZXhwIjoxOTQ5NjQwODEyfQ.4KJOyr6qyVdkxNZTkPv4NSwQIkCDqiyGdxFwnSVbdKY'
