a84af80b4a6334034d201fd86a698d2631aa1d97
cade-kreikemeier <cade@DESKTOP-70LMLPQ.localdomain>
2021-10-13 21:08:22 -0600

    Added expanded card layout

--- src/Board/JobCard.tsx
+++ src/Board/JobCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './JobCard.css';
 import { Job } from '../models/interfaces';
 
@@ -7,15 +7,63 @@ interface JobCardProps {
 }
 
 function JobCard({ job }: JobCardProps) {
-  const dueDate = (new Date(job.due_date).toLocaleDateString())
+  const [expanded, setExpanded] = useState(false);
+  const dueDate = (new Date(job.due_date).toLocaleDateString());
+  const processStatus = `
+    ${job.processes.indexOf(job.current_process) + 1}/${job.processes.length + 1}
+  `;
+
+  function toggleExpanded(e: React.MouseEvent) {
+    e.preventDefault();
+    setExpanded(!expanded);
+  };
 
   return (
-    <div className="JobCard">
+    <div className={expanded ? 'JobCardExpanded' : 'JobCard'} onDoubleClick={(toggleExpanded)}>
       <div className="CardHeader">
         <span className="JobNumber">{`j${job.id.toString().padStart(4, '0')}`}</span>
-        <span className="DueDate">due {dueDate}</span>
+        <span className="DueDate">due <span className="DateText">: {dueDate}</span></span>
       </div>
-      <span className="CustomerName">{job.customers.name}</span>
+      <span className={expanded ? 'CustomerNameExpanded' : 'CustomerName'}>
+        {expanded
+          ? <div className="CardInfo">
+            <span className="AttributeLabel"> Customer:
+              <span className="Attribute"> {job.customers.name}</span>
+            </span>
+            <span className="AttributeLabel ">Customer PO:
+              <span className="Attribute"> {job.customer_po}</span>
+            </span>
+          </div>
+          : job.customers.name}
+      </span>
+      {expanded
+        ? <>
+          <div className="CardInfo">
+            <span className="AttributeLabel">Part Name:
+              <span className="Attribute"> {job.part_name}</span>
+            </span>
+            <span className="AttributeLabel">Part Number:
+              <span className="Attribute"> {job.part_number}</span>
+            </span>
+            <span className="AttributeLabel">Part Rev:
+              <span className="Attribute"> {job.part_rev}</span>
+            </span>
+          </div>
+          <div className="CardInfo">
+            <span className="AttributeLabel Material">Material:
+              <span className="Attribute"> {job.materials?.material_name}</span>
+            </span>
+          </div>
+          <div className="CardFooter">
+            <span className="AttributeLabel JobQuantity">Qty:
+              <span className="Attribute"> {job.quantity}</span>
+            </span>
+            <span className="AttributeLabel JobProcesses">Process:
+              <span className="Attribute"> {processStatus}</span>
+            </span>
+          </div>
+        </>
+        : null}
     </div>
   );
 };
